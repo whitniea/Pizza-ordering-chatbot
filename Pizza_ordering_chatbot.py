9b11ffdb3cff0cf3f5ffc84982e25b48e7af2493
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-08-18 23:13:57 -0700

    added data validation

--- Pizza_ordering_chatbot.py
+++ Pizza_ordering_chatbot.py
@@ -2,16 +2,24 @@ print("Hello my name is Alex your virtual assistant. I will help you order a piz
 print("I am going to ask you a few questions. Please press enter after each response.")
 print("\n")
 userName = input("Enter your name:  ")
+while (len(userName) == 0):
+    userName = input("Please enter your name:   ")
 if userName.lower() == "whitnie allison":
     print("\nMy creator,  "  + userName + ". Pleasure to serve you!")
 else:
     print("Hello, " + userName + ". Nice to meet you!")
 size = input("\nWhat size do you want? Enter small, medium, or large:  ")
+while size.lower() != "small" and size.lower() != "medium" and size.lower() != "large":
+    size = input("Invalid value. Please enter small, medium, or large:   ")
 flavor = input("\nEnter the flavor of pizza:  ")
 crustType = input("\nWhat type of crust do you want:  ")
 quantity = input("\nHow many of these do you want to order:  Enter a numeric value:  ")
+while not quantity.isdigit():
+    quantity = input("\nValue not recognized. Please enter a numeric value:   ")
 quantity = int(quantity)
 method = input("\nIs this for carry out or delivery:  ")
+while method.lower() != "delivery" and method.lower() != "carry out" and method.lower() != "carryout":
+    method = input("\nInvalid value. Please enter carry out or delivery:   ")
 salesTax = 1.1
 if size.lower() == "small":
     pizzaCost = 8.99
